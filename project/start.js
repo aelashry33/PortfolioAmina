bbe8b04a9cbc03e647a7e082d176f752c5ee3562
Hayman-Laptop <aminaelashry2@gmail.com>
2020-06-24 16:09:15 -0400

    project template page

--- project/start.js
+++ project/start.js
@@ -24,10 +24,18 @@ app.get('/projects', function (request, response) {
     response.render('pages/projects');
 });
 
+app.get('/kwk', function (request, response) {
+    response.render('pages/kwk');
+});
+
+app.get('/cssi', function (request, response) {
+    response.render('pages/cssi');
+});
 
 app.get('/contact', function (request, response) {
     response.render('pages/contact');
 });
+
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
